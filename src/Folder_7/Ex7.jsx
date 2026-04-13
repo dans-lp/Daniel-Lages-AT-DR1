8a09cf47ec58bb6d50631edafd1c7a18248cd9c3
Daniel LP <54465145+dans-lp@users.noreply.github.com>
2026-04-13 03:26:21 -0300

    Add files via upload

---
+++ src/Folder_7/Ex7.jsx
@@ -0,0 +1,15 @@
+import styles from './Ex7.module.css'
+
+
+function Ex7() {
+
+   return (
+      <div className={styles.container}>
+         <div className={styles.upper}>A</div>
+         <div className={styles.upper}>B</div>
+         <div className={styles.upper}>C</div>
+      </div>
+   );
+}
+
+export default Ex7;
